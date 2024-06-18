de86b7ad74757c4a16672e502e67a03a5de6a130
Oliver Pan <2216991777@qq.com>
2024-06-18 14:10:37 -0500

    fix:vercel production file error fix

--- data.ts
+++ data.ts
@@ -1,7 +1,7 @@
 import { createSource } from "mdxts";
 
 export const allBlog = createSource("**/*.mdx", {
-  baseDirectory: "blog",
+  baseDirectory: "_blog",
   basePathname: "/blog",
   sort: (a, b) => b.frontMatter.date.localeCompare(a.frontMatter.date),
 });

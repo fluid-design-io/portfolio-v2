ddb65c539f32fc6cd9305b0cf3e6ec1c2bf0108a
Oliver Pan <2216991777@qq.com>
2024-08-31 12:04:55 -0500

    use filter replacing img texture

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import texture from "@/public/assets/images/noisy-texture.png";
 import React from "react";
 
 import { Container } from "../ui/container";
@@ -19,7 +18,6 @@ function Layout({ children, description, title }: LayoutProps) {
             <div className="mx-auto mt-16 flex h-[max(38vh,350px)] max-w-2xl items-center pb-16 lg:max-w-5xl lg:px-8">
               <PageHeader {...{ description, title }} />
             </div>
-
             <SharpPlus
               className={cn(
                 "absolute -bottom-3 -left-3 z-[2] size-6 text-muted-foreground",
@@ -34,16 +32,7 @@ function Layout({ children, description, title }: LayoutProps) {
         </div>
       </div>
       <Container>
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-0 isolate z-[-1] h-full w-full bg-repeat"
-          style={{
-            backgroundImage: `url(${texture.src})`,
-            backgroundSize: "200px",
-            mixBlendMode: "overlay",
-          }}
-        />
-        <div className="mx-auto py-16 md:py-32">{children}</div>
+        <div className={cn("mx-auto py-16 md:py-32")}>{children}</div>
       </Container>
     </>
   );
--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,6 +1,31 @@
 import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
 
+const GrainFilter = () => {
+  return (
+    <svg
+      id="svgfilters"
+      aria-hidden="true"
+      style={{ position: "absolute", width: 0, height: 0, overflow: "hidden" }}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <filter id="grain">
+          <feTurbulence baseFrequency="0.60,0.90" result="colorNoise" />
+          <feColorMatrix
+            in="colorNoise"
+            type="matrix"
+            values=".33 .33 .33 0 0 .33 .33 .33 0 0 .33 .33 .33 0 0 0 0 0 1 0"
+          />
+          <feComposite operator="in" in2="SourceGraphic" result="monoNoise" />
+          <feBlend in="SourceGraphic" in2="monoNoise" mode="multiply" />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
 const OuterContainer = forwardRef(function OuterContainer(
   {
     children,
@@ -20,6 +45,7 @@ const OuterContainer = forwardRef(function OuterContainer(
       {...props}
     >
       <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+      <GrainFilter />
     </main>
   );
 });
@@ -28,22 +54,25 @@ const InnerContainer = forwardRef(function InnerContainer(
   {
     children,
     className,
+    style,
     ...props
   }: {
     children: React.ReactNode;
     className?: string;
+    style?: React.CSSProperties;
   },
   ref: React.Ref<HTMLDivElement>,
 ) {
   return (
     <div
       className={cn(
-        "relative border-t px-4 backdrop-blur backdrop-brightness-75 sm:px-8 lg:px-12",
+        "relative border-t px-4 backdrop-blur backdrop-brightness-[0.68] sm:px-8 lg:px-12",
         // add virtical safe area padding
         "pb-[env(safe-area-inset-bottom)] pt-[env(safe-area-inset-top)]",
         className,
       )}
       ref={ref}
+      style={{ filter: "url(#grain)", ...style }}
       {...props}
     >
       <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
